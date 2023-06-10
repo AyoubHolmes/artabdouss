116eb0cbeb34ea0945765402669f2ad50da3b015
soukainahikma <soukainahikma@gmail.com>
2023-06-10 22:24:29 +0100

    feat: added picture

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
+import Image from "next/image";
 import { Test } from "../styles/EmotionStyles";
 import styles from "../styles/Home.module.css";
-
+import homeImage from "public/assets/images/61a23b69768b8481bf4248b5_katie-osullivan-hero-image copy-p-1600.jpeg"
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <div>Test 1</div>
       <Test>
-        SOUKAINA
+        Home
       </Test>
-      <div>Test 2</div>
+     <Image  src={homeImage} alt={""} height={700} width={700}/>
     </div>
   );
 };
